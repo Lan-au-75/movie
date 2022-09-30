e0844ee966088f63b9622600a58faaff5bfe8ec6
Lan <riotgamelolgare@gmail.com>
2022-09-30 20:41:17 +0700

    config tailwindcss sass font

--- src/App.js
+++ src/App.js
@@ -1,50 +1,46 @@
 import { Fragment } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import './App.css';
-
 import { publicRouter } from '~/routers';
 import DefaultLayout, { Authentication } from '~/layouts';
 
 function App() {
     return (
         <Router>
-            <div className="App">
-                <Routes>
-                    {publicRouter.map((route, index) => {
-                        const Page = route.element;
+            <Routes>
+                {publicRouter.map((route, index) => {
+                    const Page = route.element;
 
-                        let Layout;
-                        switch (route.layout) {
-                            case DefaultLayout:
-                                Layout = DefaultLayout;
-                                break;
+                    let Layout;
+                    switch (route.layout) {
+                        case DefaultLayout:
+                            Layout = DefaultLayout;
+                            break;
 
-                            case Authentication:
-                                Layout = Authentication;
-                                break;
-                            case null:
-                                Layout = Fragment;
-                                break;
+                        case Authentication:
+                            Layout = Authentication;
+                            break;
+                        case null:
+                            Layout = Fragment;
+                            break;
 
-                            default:
-                                Layout = DefaultLayout;
-                        }
+                        default:
+                            Layout = DefaultLayout;
+                    }
 
-                        return (
-                            <Route
-                                key={index}
-                                path={route.path}
-                                element={
-                                    <Layout>
-                                        <Page></Page>
-                                    </Layout>
-                                }
-                            ></Route>
-                        );
-                    })}
-                </Routes>
-            </div>
+                    return (
+                        <Route
+                            key={index}
+                            path={route.path}
+                            element={
+                                <Layout>
+                                    <Page></Page>
+                                </Layout>
+                            }
+                        ></Route>
+                    );
+                })}
+            </Routes>
         </Router>
     );
 }
